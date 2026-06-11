b7fefb5775624de970ef32c02144daf8dee9af4b
Lê Minh <leminhksqt170804@gmail.com>
2026-06-11 15:09:15 +0700

    Cập nhật nội dung 3D

--- future_studio_frontend_v2/src/data/database.ts
+++ future_studio_frontend_v2/src/data/database.ts
@@ -36,7 +36,7 @@ export const newsData: NewsItem[] = [
   { id: 2, date: '2024.03.26', title: 'Quà cưới cũng là quà tặng cho "chú rể" tốt nhất', imageUrl: 'images/logo.jpg' },
   { id: 3, date: '2026.04.05', title: 'future team tuyển dụng thành viên cho team', imageUrl: 'images/video.gif' },
   { id: 4, date: '2024.03.18', title: 'Nhìn! Future Studio GOD 👑 Giới thiệu hàng hóa dành riêng...', imageUrl: 'images/video.gif' },
-  { id: 5, date: '2024.03.10', title: 'Dự án phim hoạt hình mới chính thức bấm máy.', imageUrl: 'images/jobiterview.jpg',modelUrl: 'models/Lipstick.glb' },
+  { id: 5, date: '2024.03.10', title: 'Sản phẩm model 3D mới', imageUrl: 'images/jobiterview.jpg', modelUrl: 'models/Lipstick.glb' },
   { id: 6, date: '2024.02.28', title: 'Khai giảng khóa học 3D Animation cơ bản.', imageUrl: 'images/jobiterview.jpg' },
   { id: 7, date: '2024.02.15', title: 'Tham quan Studio: Hậu trường phía sau những thước phim.', imageUrl: 'images/jobiterview.jpg' },
   { id: 8, date: '2024.01.20', title: 'Future Studio lọt top studio sáng tạo của năm!', imageUrl: 'images/jobiterview.jpg' }
